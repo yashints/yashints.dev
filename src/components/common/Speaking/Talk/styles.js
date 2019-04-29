9ac357d72dd4ecfce153d2f3e634cd6dc6992a22
Yaser <adel.mehrban@gmail.com>
2019-04-30 08:36:19 +1000

    Added talks and fixed card style for them

--- src/components/common/Speaking/Talk/styles.js
+++ src/components/common/Speaking/Talk/styles.js
@@ -1,25 +1,26 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Wrapper = styled.div`
   display: flex;
   flex-flow: row wrap;
   padding: 1rem 0 2rem 0;
-`;
+`
 
 export const Card = styled.div`
   flex: 0 0 15%;
   min-width: 300px;
   display: flex;
   flex-direction: column;
+  background-color: #f9f9f9;
   border-radius: 8px;
   &:not(first-of-type) {
     margin-right: 1rem;
     margin-top: 15px;
   }
 
-  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
-    0 1px 1px 0 rgba(0, 0, 0, 0.14),
-    0 1px 3px 0 rgba(0, 0, 0, 0.12);
+  box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.5),
+    0 1px 1px 0 rgba(0, 0, 0, 0.5),
+    0 1px 3px 0 rgba(0, 0, 0, 0.5);
 
   img {
     margin-bottom: 0;
@@ -36,14 +37,14 @@ export const Card = styled.div`
     0 1px 3px 0 rgba(255, 255, 255, 0.12);
     
 	`};
-`;
+`
 
 export const CardLink = styled.a`
   display: block;
   min-height: 150px;
   cursor: pointer;
   color: inherit;
-`;
+`
 
 export const CardHeader = styled.div`
   font-weight: 700;
@@ -58,13 +59,13 @@ export const CardHeader = styled.div`
     `
       color: #adad2e;
   `}
-`;
+`
 
 export const CardDescription = styled.div`
   font-size: 0.9em;
   padding: 1rem;
   margin-bottom: auto;
-`;
+`
 
 export const CardDate = styled.div`
   display: flex;
@@ -78,7 +79,7 @@ export const CardDate = styled.div`
   img {
     margin-right: 10px;
   }
-`;
+`
 
 export const CardActions = styled.div`
   display: flex;
@@ -90,9 +91,9 @@ export const CardActions = styled.div`
     margin: 10px;
     cursor: pointer;
   }
-`;
+`
 
 export const Grow = styled.div`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
-`;
+`
