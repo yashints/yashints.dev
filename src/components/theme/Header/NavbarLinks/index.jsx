1f1652fa1e61f314ef579441f98af571e3671202
Yashints <adel.mehrban@gmail.com>
2020-07-31 13:36:09 +1000

    Removed services menu

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -15,9 +15,6 @@ export default ({ desktop }) => {
       <Link to="/about" activeClassName="current">
         About
       </Link>
-      <Link to="/services" activeClassName="current">
-        Services
-      </Link>
       <Link to="/speaking" activeClassName="current">
         Speaking
       </Link>
